a0d1ad6c2d2092bc9e6ad277999b5ca79a65f011
20220920 <20220920@mywhitecliffe.com>
2022-08-31 20:20:41 +1200

    new

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,16 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-st.title('Ethrium-USD 5days data by using Excel')
-df=pd.read_csv('./data/ETH-USD.csv')
-
-st.dataframe(df)
-
-st.line_chart(df['High'])
-st.bar_chart(df['Low'])
-
-fig,ax=plt.subplots()
-ax.plot(df.index, df['Adj Close'])
-ax.set_title ('matplotlib graph')
-st.pyplot(fig)
+st.title('kentaro')
+df=pd.read_csv('data.csv')
